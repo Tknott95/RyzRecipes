f1520a3fb3e41c4dd19fba431e957fc513d5390b
Trevor Knott <Tknott95@hotmail.com>
2016-06-15 15:10:07 -0600

    Recipe master list showing

---
+++ RecipeWorkspace/app/recipe-book/recipe-detail.component.js
@@ -0,0 +1,37 @@
+System.register(["angular2/core"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var RecipeDetailComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            RecipeDetailComponent = (function () {
+                function RecipeDetailComponent() {
+                }
+                RecipeDetailComponent = __decorate([
+                    core_1.Component({
+                        templateUrl: 'templates/recipes/recipe-detail.tpl.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], RecipeDetailComponent);
+                return RecipeDetailComponent;
+            }());
+            exports_1("RecipeDetailComponent", RecipeDetailComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInJlY2lwZS1ib29rL3JlY2lwZS1kZXRhaWwuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7O1lBUUE7Z0JBQUE7Z0JBRUEsQ0FBQztnQkFQRDtvQkFBQyxnQkFBUyxDQUFDO3dCQUNQLFdBQVcsRUFBRSwwQ0FBMEM7cUJBRTFELENBQUM7O3lDQUFBO2dCQUlGLDRCQUFDO1lBQUQsQ0FGQSxBQUVDLElBQUE7WUFGRCx5REFFQyxDQUFBIiwiZmlsZSI6InJlY2lwZS1ib29rL3JlY2lwZS1kZXRhaWwuY29tcG9uZW50LmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQge0NvbXBvbmVudH0gZnJvbSBcImFuZ3VsYXIyL2NvcmVcIjtcblxuQENvbXBvbmVudCh7XG4gICAgdGVtcGxhdGVVcmw6ICd0ZW1wbGF0ZXMvcmVjaXBlcy9yZWNpcGUtZGV0YWlsLnRwbC5odG1sJ1xuXG59KVxuXG5leHBvcnQgY2xhc3MgUmVjaXBlRGV0YWlsQ29tcG9uZW50IHtcblxufSJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
---
+++ RecipeWorkspace/dev/recipe-book/recipe-detail.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from "angular2/core";
+
+@Component({
+    templateUrl: 'templates/recipes/recipe-detail.tpl.html'
+
+})
+
+export class RecipeDetailComponent {
+
+}
